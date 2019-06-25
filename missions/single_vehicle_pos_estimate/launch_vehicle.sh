572d12b6279cc533094a0b7b3be8ee7bb81d5dce
caileighf <caileighfitzgerald@gmail.com>
2019-06-25 17:45:39 -0400

    wip

--- missions/single_vehicle_pos_estimate/launch_vehicle.sh
+++ missions/single_vehicle_pos_estimate/launch_vehicle.sh
@@ -2,6 +2,16 @@
 #-------------------------------------------------------
 #  Part 1: Check for and handle command-line arguments
 #-------------------------------------------------------
+
+VNAME1="jetyak"           # The first vehicle Community
+VNAME2="source"           # The second vehicle Community
+START_POS1="100,-30"         
+START_POS2="50,-100"        
+LOITER_POS1="x=0,y=-75"
+LOITER_POS2="x=50,y=-100"
+SHORE_LISTEN="9300"
+SHORE_IP="172.20.10.10"
+
 TIME_WARP=1
 JUST_MAKE="no"
 for ARGI; do
@@ -10,6 +20,10 @@ for ARGI; do
 	printf "  --just_make, -j    \n" 
 	printf "  --help, -h         \n" 
 	exit 0;
+    elif [ "${ARGI:0:11}" = "--shore-ip=" ] ; then
+        SHORE_IP="${ARGI#--shore-ip=*}"
+    elif [ "${ARGI:0:13}" = "--shore-port=" ] ; then
+        SHORE_LISTEN=${ARGI#--shore-port=*}
     elif [ "${ARGI//[^0-9]/}" = "$ARGI" -a "$TIME_WARP" = 1 ]; then 
         TIME_WARP=$ARGI
     elif [ "${ARGI}" = "--just_build" -o "${ARGI}" = "-j" ] ; then
@@ -23,15 +37,6 @@ done
 #-------------------------------------------------------
 #  Part 2: Create the .moos and .bhv files. 
 #-------------------------------------------------------
-VNAME1="jetyak"           # The first vehicle Community
-VNAME2="source"           # The second vehicle Community
-START_POS1="100,-30"         
-START_POS2="50,-100"        
-LOITER_POS1="x=0,y=-75"
-LOITER_POS2="x=50,y=-100"
-SHORE_LISTEN="9300"
-
-
 nsplug meta_vehicle.moos targ_$VNAME1.moos -f WARP=$TIME_WARP \
     VNAME=$VNAME1          SHARE_LISTEN="9301"              \
     VPORT="9001"           SHORE_LISTEN=$SHORE_LISTEN       \
@@ -61,7 +66,7 @@ printf "Launching $VNAME2 MOOS Community (WARP=%s) \n" $TIME_WARP
 pAntler targ_$VNAME2.moos >& /dev/null &
 printf "Done \n"
 
-uMAC targ_${VNAME}.moos
+uMAC targ_jetyak.moos
 
 printf "Killing all processes ... \n"
 kill %1 %2 %3
